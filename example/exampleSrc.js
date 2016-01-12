63b64204581c9e885f0701f46bb65cf4f50dd4e6
Matthew Poulson <matthewpoulson@ldschurch.org>
2016-01-12 16:57:06 -0700

    init example

---
+++ example/exampleSrc.js
@@ -0,0 +1,3 @@
+import stickify from '../index';
+
+stickify();
---
+++ example/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+	npm = require('rollup-plugin-npm'),
+	commonjs = require('rollup-plugin-commonjs'),
+	// gulp-load-plugins lets you lazyload plugins without a giant list of
+	// `require` statements...
+	$ = require('gulp-load-plugins')({pattern: ['gulp-*', 'gulp.*', '*']});
+
+
+
+// Application Scripts
+gulp.task('clean', function(cb) {
+	$.del(['examplePublic.js'], cb);
+});
+
+gulp.task('lint', function() {
+	return gulp.src(['exampleSrc.js'])
+		// .pipe($.cached('js'))
+    	.pipe($.jshint())
+    	.pipe($.jshint.reporter('jshint-stylish'))
+    	.pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('build', ['lint', 'clean'], function(){
+
+	return gulp.src('exampleSrc.js')
+	// return gulp.src(['src/layouts/**/*.js', 'src/document-templates/**/*.js'])
+		// .pipe( $.concat('./layouts/app.js') ) // #1 comment this to NOT concatenate scripts
+		// .pipe( $.es6ModuleTranspiler({ formatter: 'bundle' }))
+		.pipe( $.rollup({
+			format: 'iife',
+			plugins: [npm(), commonjs()]
+		}) )
+		.pipe( $.babel() )
+		.pipe( $.rename('examplePublic.js'))
+		// .pipe($.flatten() ) // <-- #2 uncomment this to NOT concatenate scripts
+		.pipe( gulp.dest('/'));
+});
